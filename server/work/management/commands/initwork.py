ee68b39ae4f37d2d6cc0904ff06f33e9d08f8dc7
Floor Terra <floort@gmail.com>
2012-04-28 16:42:09 +0200

    Added work app

---
+++ server/work/management/commands/initwork.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from server.work.models import *
+from itertools import combinations_with_replacement
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Init work database.'
+
+    def handle(self, *args, **options):
+        for block in combinations_with_replacement(range(256), 3):
+            block_str = "%d.%d.%d" % (block[0], block[1], block[2])
+            t = Task(bucket = block_str)
+            t.save()
---
+++ server/work/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    bucket = models.CharField(max_length=12, db_index=True)
+    status = models.CharField(  max_length=4, 
+                                db_index=True,
+                                choices = (
+                                    ("O", "Open"),
+                                    ("IP", "In Progress"),
+                                    ("F", "Finished")
+                                ),
+                                default = "O")
+    started = models.DateTimeField(null=True, blank=True)
+    finished = models.DateTimeField(null=True, blank=True)
+    worker_id = models.CharField(max_length=64, null=True, blank=True)
+
+
+
