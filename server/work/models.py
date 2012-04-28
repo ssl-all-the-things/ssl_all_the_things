b16c02d9a634e69a717834387f2add3da1d865da
Floor Terra <floort@gmail.com>
2012-04-28 17:42:49 +0200

    Make bucket unique

--- server/work/models.py
+++ server/work/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-    bucket = models.CharField(max_length=12, db_index=True)
+    bucket = models.CharField(max_length=12, db_index=True, unique=True)
     status = models.CharField(  max_length=4, 
                                 db_index=True,
                                 choices = (
