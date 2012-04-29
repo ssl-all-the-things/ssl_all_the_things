2c3134da6267a26619eedb0d9b867dc44fffc9cb
Oscar Koeroo <okoeroo@gmail.com>
2012-04-29 12:58:06 +0200

    Made it work.

--- worker/job.sh
+++ worker/job.sh
@@ -6,25 +6,25 @@ CONTROL_PORT="8000"
 CONNECT_BASE="http://${CONTROL_IP}:${CONTROL_PORT}"
 
 
-function done_ipblock(){
-    curl "${CONNECT_BASE}/done"
-}
-
-
 WORKER_ID=$(echo "`hostname`$$" | md5sum | cut -f1 -d" ")
 echo "Worker ID: $WORKER_ID"
 
 while [ 1 ]; do
     # Fetch new work
-    #BLOB=$(curl "${CONNECT_BASE}/get/`hostname`/" 2>/dev/null)
-    #echo $?
-    #echo $BLOB
 
-    curl "${CONNECT_BASE}/get/${WORKER_ID}/" || exit 1 | while read line ; do
+    WORK_FILE="/tmp/${WORKER_ID}.work"
+    curl "${CONNECT_BASE}/get/${WORKER_ID}/" > ${WORK_FILE} 2>/dev/null || continue
+
+    cat ${WORK_FILE} | while read line ; do
         ./worker.sh $line
-    done || continue
+    done
+    rm ${WORK_FILE}
 
-    # curl --data "$line"  "${CONNECT_BASE}/done/${WORKER_ID}/"
+    curl --data "'$line'"  "${CONNECT_BASE}/done/${WORKER_ID}/"
     sleep 2
 
 done
+
+exit 0
+
+
