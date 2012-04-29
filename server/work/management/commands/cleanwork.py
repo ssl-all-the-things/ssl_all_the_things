78452e30805d8ebf367a818ce5c2361843d117a0
Floor Terra <floort@gmail.com>
2012-04-29 15:15:25 +0200

    Give jobs 45 minutes to finish

--- server/work/management/commands/cleanwork.py
+++ server/work/management/commands/cleanwork.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         t = Task.objects.filter(status="IP")
-        t = t.filter(started__lt=datetime.datetime.now()-datetime.timedelta(minutes=15))
+        t = t.filter(started__lt=datetime.datetime.now()-datetime.timedelta(minutes=45))
         t.update(status="O")
         print "%d Stale jobs cleared." % (len(t), )
         return None
