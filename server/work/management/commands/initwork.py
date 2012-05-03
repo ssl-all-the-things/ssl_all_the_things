5ff70f60abdbe459e28173f50ccc25f99b725303
Floor Terra <floort@gmail.com>
2012-05-04 00:59:14 +0200

    Changed to 12 byte sized blocks

--- server/work/management/commands/initwork.py
+++ server/work/management/commands/initwork.py
@@ -8,12 +8,13 @@ class Command(BaseCommand):
     help = 'Init work database.'
 
     def handle(self, *args, **options):
-        for block in combinations_with_replacement(range(256), 3):
+        for block in combinations_with_replacement(range(256), 2):
             # Filter RFC 1918
             if block[0] == 10: continue
             if block[0] == 127 and block[1] > 15 and block[1] < 32: continue
             if block[0] == 192 and block[1] == 168: continue
             if block[0] == 0: continue
-            block_str = "%d %d %d" % (block[0], block[1], block[2])
-            t = Task(bucket = block_str)
-            t.save()
+            for c in range(0, 256, 16):
+                block_str = "%d %d %d" % (block[0], block[1], c)
+                t = Task(bucket = block_str)
+                t.save()
